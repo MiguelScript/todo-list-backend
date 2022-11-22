bb474e28ae7ea5791f5a66556c5567cf3c98021b
Miguel <acostalugo.m@gmail.com>
2022-11-22 12:06:29 -0400

    agregar .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/todo-list-app', {
       useNewUrlParser: true,
     }),
+    ConfigModule.forRoot(),
     TasksModule,
   ],
   controllers: [AppController],
